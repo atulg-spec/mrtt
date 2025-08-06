d26173eed81f4977b364c183372f02143dee2083
atul <atulg0736@gmail.com>
2025-08-07 01:43:51 +0530

    debug off

--- treedonation/settings.py
+++ treedonation/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8zr*+w9tfq)gfs3#u*7xt-7k@vh3pk_tcwpa47c148i8uy%m0-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -165,11 +165,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = 'static'
+STATIC_ROOT = 'static'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/media/')
