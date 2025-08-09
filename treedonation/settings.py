481ae5f57135c040afa1eef9dd3500fda197bb35
atul <atulg0736@gmail.com>
2025-08-09 11:17:47 +0530

    iky

--- treedonation/settings.py
+++ treedonation/settings.py
@@ -85,12 +85,24 @@ WSGI_APPLICATION = 'treedonation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mrtt_database',
+        'USER': 'mrtt_user',
+        'PASSWORD': '@MrtFoundationGroup223',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
