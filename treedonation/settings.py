0f2dfced3b5f3d164f482602957e69d68d57dba8
atul <atulg0736@gmail.com>
2025-08-19 00:13:30 +0530

    some fixes d

--- treedonation/settings.py
+++ treedonation/settings.py
@@ -89,23 +89,23 @@ DATE_INPUT_FORMATS = ['%d/%m/%Y']
 DATE_FORMAT = 'd/m/Y'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mrtt_database',
-#         'USER': 'mrtt_user',
-#         'PASSWORD': '@MrtFoundationGroup223',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mrtt_database',
+        'USER': 'mrtt_user',
+        'PASSWORD': '@MrtFoundationGroup223',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 AUTHENTICATION_BACKENDS = [
