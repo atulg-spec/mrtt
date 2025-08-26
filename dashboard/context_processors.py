98be553583ae12a6b5355cbabf8125132af6ae68
atul <atulg0736@gmail.com>
2025-08-26 20:13:45 +0530

    some fix

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -4,28 +4,26 @@ from django.db.models import Count
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 
-
 def custom_admin_context(request):
     from accounts.models import CustomUser, SelfieWithTree
     from donation.models import ManualPayment, Donation, Registration_fee
 
     context = {}
     if request.path.startswith('/admin/') and 'login' not in request.path:
-        # Aggregate all necessary values in one query
         pending_manual_payments = ManualPayment.objects.filter(status='PENDING').count() or 0
         total_users = CustomUser.objects.count()
-        
         total_donations = round(Donation.objects.aggregate(total=models.Sum('amount'))['total'] or 0, 2)
         unverified_series = SelfieWithTree.objects.filter(status='pending').count() or 0
         total_registration_fees = round(Registration_fee.objects.aggregate(total=models.Sum('amount'))['total'] or 0, 2)
-        # Optimized query for chart data using `values` and `annotate`
-        chart_data = CustomUser.objects.all() \
-    .annotate(date=TruncDay("date_joined")) \
-    .values("date") \
-    .annotate(y=Count("id")) \
-    .order_by("date")
 
-        # Serialize chart data to JSON
+        # Chart data (x = date, y = number of users)
+        chart_data = (
+            CustomUser.objects.annotate(x=TruncDay("date_joined"))
+            .values("x")
+            .annotate(y=Count("id"))
+            .order_by("x")
+        )
+
         context = {
             "total_users": total_users,
             "total_donations": total_donations,
@@ -34,5 +32,4 @@ def custom_admin_context(request):
             "total_registration_fees": total_registration_fees,
             "chart_data": json.dumps(list(chart_data), cls=DjangoJSONEncoder),
         }
-
     return context
