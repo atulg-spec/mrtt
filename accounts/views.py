8704e85fd78ba0238fe777018b07f33d61a78b57
atul <atulg0736@gmail.com>
2025-09-14 18:31:37 +0530

    location

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,34 @@ from django.contrib.auth.decorators import login_required
 from accounts.utils import phone_number_required
 from .forms import PhoneNumberForm, KYCVerificationForm
 from django.contrib.auth.forms import SetPasswordForm
+from django.views import View
+import json
+from django.utils.decorators import method_decorator
+from django.http import JsonResponse
+
+class UpdateUserLocationView(View):
+    @method_decorator(login_required)  # Ensures that the user is logged in
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+            user = request.user  # Get the logged-in user
+
+            # Update the user model with the new location data
+            user.region_name = data.get('region_name', user.region_name)
+            user.city = data.get('city', user.city)
+            user.zip_code = data.get('zip_code', user.zip_code)
+            user.lat = data.get('lat', user.lat)
+            user.lon = data.get('lon', user.lon)
+            user.timezone = data.get('timezone', user.timezone)
+            user.isp = data.get('isp', user.isp)
+
+            user.save()  # Save the changes to the user model
+
+            return JsonResponse({"message": "User data updated successfully"}, status=200)
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+
 
 def google_login_redirect(request):
     ref = request.GET.get('ref')
