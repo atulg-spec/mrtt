226f97e53a84dd5b1a23179ed2d93501003d1875
atul <atulg0736@gmail.com>
2025-09-14 18:38:57 +0530

    location auto

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,12 +3,11 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('update-user-location/', views.UpdateUserLocationView.as_view(), name='update_user_location'),
     path('phone-number-registration/', views.phone_number_registration, name='phone_number_registration'),
     path('kyc-verification/', views.kyc_verification, name='kyc_verification'),
     path('login/', views.login, name='login'),
-
     path("password/set/", views.set_password_view, name="set_password"),
     # path("password/set/done/", views.set_password_done, name="password_set_done"),
-
     path('logout/', views.logout, name='logout'),
 ]
