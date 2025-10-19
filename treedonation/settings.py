f246b24e894d988e6bdcbe513181c8bcfbeb201a
atul <atulg0736@gmail.com>
2025-10-19 11:05:18 +0530

    tsl off

--- treedonation/settings.py
+++ treedonation/settings.py
@@ -205,7 +205,7 @@ PAPER_ENVIRONMENT_COLOR = "#09D320"
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = 'atulg0736@gmail.com'
 EMAIL_HOST_PASSWORD = 'gbka bvuh nxrl matz'
 DEFAULT_FROM_EMAIL = 'MRT Green Foundation <no-reply@mrtt.org.in>'
